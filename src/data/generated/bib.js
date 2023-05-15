f6e4889d83c57dead35dfdbce40e9a4998a3bfc8
colintkh-uon <126162272+colintkh-uon@users.noreply.github.com>
2023-05-15 17:01:35 +0100

    first push
    Update

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -12,6 +12,116 @@
         "type": "article",
         "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
         "volume": "22",
-        "year": "2016"
+        "year": "2016 ,"
+    },
+    "Dehghan2022": {
+        "author": "Dehghan, Pegah and Alashwal, Hany and Moustafa, Ahmed",
+        "doi": "10.1007/s44202-022-00027-5 ,",
+        "journal": "Discover Psychology",
+        "month": "12",
+        "pages": "",
+        "title": "Applications of machine learning to behavioral sciences: focus on categorical data",
+        "type": "article",
+        "volume": "2",
+        "year": "2022"
+    },
+    "Godfrey2019": {
+        "author": "Godfrey, Erin and Burson, Esther and Yanisch, Tess and Hughes, Diane and Way, Niobe",
+        "doi": "10.1037/dev0000558 ,",
+        "journal": "Developmental psychology",
+        "month": "03",
+        "pages": "525-537",
+        "title": "A bitter pill to swallow? Patterns of critical consciousness and socioemotional and academic well-being in early adolescence",
+        "type": "article",
+        "volume": "55",
+        "year": "2019"
+    },
+    "Kermott2019": {
+        "author": "Kermott, Cindy and Johnson, Ruth and Sood, Richa and Jenkins, Sarah and Sood, Amit",
+        "doi": "10.1371/journal.pone.0218092 ,",
+        "journal": "PLOS ONE",
+        "month": "06",
+        "pages": "e0218092",
+        "title": "Is higher resilience predictive of lower stress and better mental health among corporate executives?",
+        "type": "article",
+        "volume": "14",
+        "year": "2019"
+    },
+    "Kosinski2013": {
+        "author": "Kosinski, Michal and Stillwell, David and Graepel, Thore",
+        "doi": "10.1073/pnas.1218772110 ,",
+        "journal": "Proceedings of the National Academy of Sciences of the United States of America",
+        "month": "03",
+        "pages": "",
+        "title": "Private traits and attributes are predictable from digital records of human behavior",
+        "type": "article",
+        "volume": "110",
+        "year": "2013"
+    },
+    "RADZI2021": {
+        "author": "RADZI, SITI and Hassan, Mohd and Radzi, Muhammad",
+        "doi": "10.21203/rs.3.rs-156315/v1 ,",
+        "month": "01",
+        "pages": "",
+        "title": "Comparison of Classification Algorithms for Predicting Autistic Spectrum Disorder Using WEKA Modeler",
+        "type": "unknown",
+        "year": "2021"
+    },
+    "Rosenbusch2019": {
+        "author": "Rosenbusch, Hannes and Soldner, Felix and Evans, Anthony and Zeelenberg, Marcel",
+        "doi": "10.31234/osf.io/s72vu ,",
+        "month": "09",
+        "pages": "",
+        "title": "Supervised machine learning methods in psychology: A practical introduction with annotated R code",
+        "type": "unknown",
+        "year": "2019"
+    },
+    "Rothenberg2023": {
+        "author": "Rothenberg, William and Bizzego, Andrea and Esposito, Gianluca and Lansford, Jennifer and Al-Hassan, Suha and Bacchini, Dario and Chang, Lei and Deater-Deckard, Kirby and Giunta, Laura and Dodge, Kenneth and Gurdal, Sevtap and Liu, Qin and Long, Qian and Oburu, Paul and Pastorelli, Concetta and Skinner, Ann and Sorbring, Emma and Tapanya, Sombat and Alampay, Liane",
+        "doi": "10.1007/s10964-023-01767-w ,",
+        "journal": "Journal of Youth and Adolescence",
+        "month": "04",
+        "pages": "1-25",
+        "title": "Predicting Adolescent Mental Health Outcomes Across Cultures: A Machine Learning Approach",
+        "type": "article",
+        "year": "2023"
+    },
+    "Ungar2008": {
+        "abstract": "\"{Findings from a 14 site mixed methods study of over 1500 youth globally support four propositions that underlie a more culturally and contextually embedded understanding of resilience: 1) there are global, as well as culturally and contextually specific aspects to young people\u2019s lives that contribute to their resilience; 2) aspects of resilience exert differing amounts of influence on a child\u2019s life depending on the specific culture and context in which resilience is realized; 3) aspects of children\u2019s lives that contribute to resilience are related to one another in patterns that reflect a child\u2019s culture and context; 4) tensions between individuals and their cultures and contexts are resolved in ways that reflect highly specific relationships between aspects of resilience. The implications of this cultural and contextual understanding of resilience to interventions with at-risk populations are discussed.}\",",
+        "author": "Ungar, Michael",
+        "doi": "10.1093/bjsw/bcl343",
+        "eprint": "https://academic.oup.com/bjsw/article-pdf/38/2/218/4719916/bcl343.pdf ,",
+        "issn": "0045-3102",
+        "journal": "The British Journal of Social Work",
+        "month": "10",
+        "number": "2",
+        "pages": "218-235",
+        "title": "\"{Resilience across Cultures}\",",
+        "type": "article",
+        "url": "https://doi.org/10.1093/bjsw/bcl343",
+        "volume": "38",
+        "year": "2006"
+    },
+    "Ungar2019": {
+        "author": "Ungar, Michael and Theron, Linda",
+        "doi": "10.1016/S2215-0366(19)30434-1 ,",
+        "journal": "The Lancet Psychiatry",
+        "month": "12",
+        "pages": "",
+        "title": "Resilience and mental health: how multisystemic processes contribute to positive outcomes",
+        "type": "article",
+        "volume": "7",
+        "year": "2019"
+    },
+    "Weidman2023": {
+        "author": "Weidman, Sarah and Levine, Diane and Louwagie, Fransiska and Blackmore, Kara and Theron, Linda and Stekel, Dov",
+        "doi": "10.1007/s12144-023-04578-1",
+        "journal": "Current Psychology",
+        "month": "04",
+        "pages": "",
+        "title": "The role of multisystemic resilience in fostering critical agency: UK adolescents during the COVID-19 Pandemic",
+        "type": "article",
+        "volume": "2023",
+        "year": "2023"
     }
 };
